7834fc8fc3d054ef5d76c5ceef17922f461ca79c
yinmotion <yinmotion.nyc@gmail.com>
2011-12-16 18:15:08 -0500

    updates

--- MTAmashup/src/com/yinmotion/MTAmashup/MTAmashupActivity.java
+++ MTAmashup/src/com/yinmotion/MTAmashup/MTAmashupActivity.java
@@ -2,11 +2,13 @@ package com.yinmotion.MTAmashup;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MTAmashupActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    	requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
