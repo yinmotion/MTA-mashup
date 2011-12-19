a255275cca2f395caecece86de5e76836796408e
yinmotion <yinmotion.nyc@gmail.com>
2011-12-19 16:49:32 -0500

    add load xml functions

--- MTAmashup/src/com/yinmotion/MTAmashup/MTAmashupActivity.java
+++ MTAmashup/src/com/yinmotion/MTAmashup/MTAmashupActivity.java
@@ -1,15 +1,33 @@
 package com.yinmotion.MTAmashup;
 
+import org.w3c.dom.Document;
+
+import com.yinmotion.MTAmashup.R.string;
+
 import android.app.Activity;
 import android.os.Bundle;
+
+import android.util.Log;
 import android.view.Window;
 
 public class MTAmashupActivity extends Activity {
     /** Called when the activity is first created. */
+	public static final String PATH_MTA_STATUS = "http://www.mta.info/status/serviceStatus.txt";
+	public static final String TAG = "MTAmashupActivity";
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
     	requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        String xml = XMLfunctions.getXML();
+        Document doc = XMLfunctions.XMLfromString(xml);
+          
+        Log.v(TAG, "xml = "+xml);
+        //int numResults = XMLfunctions.numResults(doc);
     }
+    
+    
+    
 }
